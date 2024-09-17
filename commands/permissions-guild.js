9af0e20f7c25bb409cd2e8c8fe09df2ee4edfdf0
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-17 22:10:44 +0530

    Update and rename permissions.js to permissions-guild.js

--- commands/permissions-guild.js
+++ commands/permissions-guild.js
@@ -2,24 +2,26 @@ const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('permissions')
-        .setDescription('Check which permissions the bot has in this channel'),
+        .setName('guildpermissions')
+        .setDescription('Check which permissions the bot has in the server'),
     
     async execute(interaction) {
-      
+       
         const botMember = await interaction.guild.members.fetch(interaction.client.user.id);
-        const channelPermissions = botMember.permissionsIn(interaction.channel);
 
 
+        const guildPermissions = botMember.permissions;
+
+  
         const permissions = Object.keys(PermissionsBitField.Flags)
-            .filter(perm => channelPermissions.has(perm));
+            .filter(perm => guildPermissions.has(perm));
 
- 
+      
         const permissionsList = permissions.map(perm => `\`${perm}\``).join(', ');
 
-
+       
         await interaction.reply({
-            content: `Here are the permissions I have in this channel: ${permissionsList || 'No permissions.'}`,
+            content: `Here are the permissions I have in this server: ${permissionsList || 'No permissions.'}`,
             ephemeral: true, 
         });
     },
