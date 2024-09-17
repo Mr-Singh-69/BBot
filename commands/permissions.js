7a75e85f182eb5418754fe741bd37e5a8bf21137
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-17 12:50:42 +0530

    Update permissions.js

--- commands/permissions.js
+++ commands/permissions.js
@@ -1,1 +1,25 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('permissions')
+        .setDescription('Check which permissions the bot has in this channel'),
+    
+    async execute(interaction) {
+        const botMember = await interaction.guild.members.fetch(interaction.client.user.id);
+        const channelPermissions = botMember.permissionsIn(interaction.channel);
+
+  
+        const permissions = Object.keys(PermissionsBitField.Flags)
+            .filter(perm => channelPermissions.has(perm));
+
+
+        const permissionsList = permissions.map(perm => `\`${perm}\``).join(', ');
+
+
+        await interaction.reply({
+            content: `Here are the permissions I have in this channel: ${permissionsList || 'No permissions.'}`,
+            ephemeral: true,  
+    },
+};
 
