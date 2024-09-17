0cb37360569b9dd348e8081bbca6e04e94f6fcbe
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-17 12:55:34 +0530

    Update permissions.js

--- commands/permissions.js
+++ commands/permissions.js
@@ -6,20 +6,21 @@ module.exports = {
         .setDescription('Check which permissions the bot has in this channel'),
     
     async execute(interaction) {
+      
         const botMember = await interaction.guild.members.fetch(interaction.client.user.id);
         const channelPermissions = botMember.permissionsIn(interaction.channel);
 
-  
+
         const permissions = Object.keys(PermissionsBitField.Flags)
             .filter(perm => channelPermissions.has(perm));
 
-
+ 
         const permissionsList = permissions.map(perm => `\`${perm}\``).join(', ');
 
 
         await interaction.reply({
             content: `Here are the permissions I have in this channel: ${permissionsList || 'No permissions.'}`,
-            ephemeral: true,  
+            ephemeral: true, 
+        });
     },
 };
-
