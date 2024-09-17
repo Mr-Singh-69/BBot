d3cd86b19a2e89d7309f5e5dd9b852f2ef5f763f
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-17 22:23:07 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -85,6 +85,14 @@ module.exports = {
             } else {
                 message.channel.send(`I win! I chose ${botChoice}.`);
             }
+else if (command === 'guildpermissions') {
+            
+            const botMember = await message.guild.members.fetch(message.client.user.id);
+            const guildPermissions = botMember.permissions;
+            const permissions = Object.keys(PermissionsBitField.Flags)
+                .filter(perm => guildPermissions.has(perm));
+            const permissionsList = permissions.map(perm => `\`${perm}\``).join(', ');
+            message.channel.send(`Here are the permissions I have in this server: ${permissionsList || 'No permissions.'}`);
         }
     },
 };
