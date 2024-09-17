85a1b5d3ee74b7296a1e7d9ce295f4e2067b0cee
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-17 22:31:49 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,5 @@
 const prefix = "#";
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
     name: 'messageCreate',
@@ -85,13 +86,22 @@ module.exports = {
             } else {
                 message.channel.send(`I win! I chose ${botChoice}.`);
             }
+        }
+
+       
         else if (command === 'guildpermissions') {
             
             const botMember = await message.guild.members.fetch(message.client.user.id);
             const guildPermissions = botMember.permissions;
+
+            
             const permissions = Object.keys(PermissionsBitField.Flags)
                 .filter(perm => guildPermissions.has(perm));
+
+            
             const permissionsList = permissions.map(perm => `\`${perm}\``).join(', ');
+
+            
             message.channel.send(`Here are the permissions I have in this server: ${permissionsList || 'No permissions.'}`);
         }
     },
